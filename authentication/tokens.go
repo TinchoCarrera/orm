f4abbf9f76a63001b703faaacf71cfec294af495
TinchoCarrera <mctcarrera@gmail.com>
2023-02-24 13:51:22 -0300

    Subida 24/02 13:51

--- authentication/tokens.go
+++ authentication/tokens.go
@@ -40,4 +40,24 @@ func ValidateToken(w http.ResponseWriter, r *http.Request) bool {
 
 	fmt.Println(tokenString)
 	return true
+	/*
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			// Don't forget to validate the alg is what you expect:
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+
+			// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+			return os.Getenv("CLAVESECRETATOKEN"), nil
+		})
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			fmt.Println(claims["foo"], claims["nbf"])
+			return true
+		} else {
+			fmt.Println(err.Error())
+			return false
+		}
+	*/
+
 }
